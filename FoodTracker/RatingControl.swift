9709d964d0ba63aac2d1f9ace9e110091f32a321
Mariana Diogo <marianadiogo.dev@gmail.com>
2020-01-16 23:11:37 +0100

    Added Accessibility Information

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -12,7 +12,11 @@ import UIKit
     
     //MARK: Properties
     private var ratingButtons = [UIButton]()
-    var rating = 0
+    var rating = 0 {
+        didSet {
+            updateButtonSelectionStates()
+        }
+    }
     
     @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
         didSet {
@@ -38,7 +42,14 @@ import UIKit
     
     //MARK: Button Action
     @objc func ratingButtonTapped(button: UIButton) {
-        print("Button pressed 👍")
+        guard let index = ratingButtons.firstIndex(of: button) else {
+            fatalError("The button \(button) is not present in the rating buttons array: \(ratingButtons)")
+        }
+        
+        //If there is no rating, update the rating value, otherwise reset it
+        let selectedRating = index + 1
+        rating = selectedRating == rating ? 0 : selectedRating
+        
     }
     
     //MARK: Private Methods
@@ -57,13 +68,15 @@ import UIKit
         let highlightedStar = UIImage(named: "highlightedStar", in: bundle, compatibleWith: self.traitCollection)
         
         //Create the new buttons
-        for _ in 0..<starCount {
+        for index in 0..<starCount {
             let button = UIButton()
             button.setImage(emptyStar, for: .normal)
             button.setImage(filledStar, for: .selected)
             button.setImage(highlightedStar, for: .highlighted)
             button.setImage(highlightedStar, for: [.highlighted, .selected])
             
+            // Accessibilit
+            button.accessibilityLabel = "Set \(index + 1) star rating"
             
             // Add button constraints
             button.translatesAutoresizingMaskIntoConstraints = false
@@ -82,6 +95,37 @@ import UIKit
             // Add the new button to the rating button array
             ratingButtons.append(button)
         }
-    }
 
+        updateButtonSelectionStates()
+    }
+    
+    private func updateButtonSelectionStates() {
+        
+        for (index, button) in ratingButtons.enumerated() {
+            button.isSelected = index < rating
+            
+            // Set the hint string for the currently selected star
+            let hintString: String?
+            if rating == index + 1 {
+                hintString = "Tap to reset the rating to zero."
+            } else {
+                hintString = nil
+            }
+            
+            // Calculate the value string
+            let valueString: String
+            switch (rating) {
+                case 0:
+                    valueString = "No rating set."
+                case 1:
+                    valueString = "1 star set."
+                default:
+                    valueString = "\(rating) stars set."
+            }
+            // Assign the hint string and value string
+            button.accessibilityHint = hintString
+            button.accessibilityValue = valueString
+            
+        }
+    }
 }
